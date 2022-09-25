02b7bc40f8ac0a45dfef692e5888766a9019ef39
Christian <chrisnebech@hotmail.com>
2022-09-25 16:45:47 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa ao entrar na página /favorites', () => {
+  test('é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const text = screen.getByText(/no favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+});
